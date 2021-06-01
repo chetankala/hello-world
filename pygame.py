7cd98940c96ad1ca71ff8b0b1b86aa2db49c0614
chetankala <56772558+chetankala@users.noreply.github.com>
2021-05-31 23:59:35 -0400

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,33 @@
+import pygame
+import random 
+
+pygame.init()
+winHeight = 480
+winWidth = 700
+win=pygame.display.set_mode((winWidth,winHeight))
+#---------------------------------------#
+# initialize global variables/constants #
+#---------------------------------------#
+BLACK = (0,0, 0)
+WHITE = (255,255,255)
+RED = (255,0, 0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+LIGHT_BLUE = (102,255,255)
+
+btn_font = pygame.font.SysFont("ariel", 20)
+guess_font = pygame.font.SysFont("monospace", 24)
+lost_font = pygame.font.SysFont('ariel', 45)
+word = ''
+buttons = []
+guessed = []
+hangmanPics = [pygame.image.load('hangman0.png'), pygame.image.]
+
+limbs = 0
+
+
+def redraw_game_window():
+	global guessed 
+	global hangmanPics
+	global limbs 
+	win.fill(GREEN)
